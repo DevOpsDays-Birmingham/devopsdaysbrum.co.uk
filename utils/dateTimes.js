3a62fecabeda891b4fb025d5d48944d96430febb
Alistair Hey <alistair@heyal.co.uk>
2023-01-31 09:13:26 +0000

    Fix month number
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- utils/dateTimes.js
+++ utils/dateTimes.js
@@ -1,5 +1,6 @@
 export function getMonthName(monthNumber) {
     const date = new Date();
+    date.setDate(1)
     date.setMonth(monthNumber - 1);
 
     return date.toLocaleString('en-UK', { month: 'long' });
